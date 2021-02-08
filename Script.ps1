dfee0e70fd0ef7d13abb6e22b4cee5ada5fddee2
A. S <sergio.as@atos.net>
2021-02-08 15:42:08 -0600

    initial commit

---
+++ Script.ps1
@@ -0,0 +1,9 @@
+$Data = Import-Csv ".\Input\Input.csv"
+$Counter = 1
+$Data | ForEach-Object{
+    $Server = $_.Server
+    $perc=[math]::round(($Counter/($Data).Count)*100)
+    Write-progress -id 1 -percent $perc -activity "Looking up $($Server)"    
+    Start-Sleep -Milliseconds 500
+    $Counter++
+}
