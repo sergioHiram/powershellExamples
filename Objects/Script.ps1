13ea37cd6badc94a3aefdb533ee197d9eb93b83b
hiram@shopphi.xyz <hiram@shopphi.xyz>
2021-06-07 22:32:10 -0500

    Objectos

--- Objects/Script.ps1
+++ Objects/Script.ps1
@@ -14,14 +14,15 @@ $ObjetoNuevo.Area
 
 #Creando objeto apartir de Get-WMIObject
 $Data = Get-WmiObject -Class Win32_ComputerSystem | Select-Object -Property * #Información del sistema
-$DataNetwork = Get-WmiObject -Class Win32_NetworkAdapterConfiguration #Información del NIC de red
+$DataNetwork = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object -property Index -eq 1 #Información del NIC de red
 $ObjetoSistema = New-Object -TypeName PSCustomObject #Creando el objeto nuevo
 
 #Agregando propiedades y valores al objeto nuevo
 Add-Member -InputObject $ObjetoSistema -MemberType NoteProperty -Name Domain -Value ($Data.Domain) -Force
 Add-Member -InputObject $ObjetoSistema -MemberType NoteProperty -Name Manufacturer -Value ($Data.Manufacturer) -Force
+Add-Member -InputObject $ObjetoSistema -MemberType NoteProperty -Name Ip -Value ($DataNetwork.IPAddress[0]) -Force
+Add-Member -InputObject $ObjetoSistema -MemberType NoteProperty -Name NetworkDescription -Value ($DataNetwork.Description) -Force
 
 
 
-
-$ObjetoSistema.Manufacturer
+$ObjetoSistema.Ip
